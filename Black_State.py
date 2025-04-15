d15c25d172f2ca5788dac1e0e8ad2ebd18c71bb5
Asun2112 <Asunkam12@gmail.com>
2025-04-14 22:32:07 -0500

    Update Black_State.py

--- Black_State.py
+++ Black_State.py
@@ -1,11 +1,21 @@
 'States: 0:white, 1:light_blue, 2: Dark_blue, 3: red, 4:black'
+from state_class import state
 
-state=4
+class Black(state):
+    state = 4
 
-def action(self, i, j):
-    if self.state in self.neighbor:
-        return True
+    def action(self):
+        neighbor_states = list(self.neighbhorhood[0])
+        neighbor_locs = list(self.neighbhorhood[1])
+        
+        if 1 in neighbor_states:
+            light_blue_index = neighbor_states.index(1)
+            
+            count_black = 0
+            for i, s in enumerate(neighbor_states):
+                if s == 4 and i != light_blue_index:
+                    count_black += 1
 
-    elif 1 in self.neighborhood:
-        index_of = self.neighbhorhood.index(1)
-    return False, index_of
+            if count_black == 0:
+                return True, light_blue_index 
+        return False, None
