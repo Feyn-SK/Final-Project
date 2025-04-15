0f99717e5ece90fd79b6e049424c52d28073fe16
Venuk853 <84994112+Venuk853@users.noreply.github.com>
2025-04-15 16:38:54 -0500

    Update Black_State.py

--- Black_State.py
+++ Black_State.py
@@ -1,21 +1,12 @@
 'States: 0:white, 1:light_blue, 2: Dark_blue, 3: red, 4:black'
-from state_class import state
+from Neighborhood import neighbor as nbh
+from State_Class import State
 
-class Black(state):
-    state = 4
+class Black(State):
+    def __init__(self, row, col, parent_array, state = 2):
+        super().__init__(row, col, parent_array, state = 2)  # Initialize parent class attributes
 
-    def action(self):
-        neighbor_states = list(self.neighbhorhood[0])
-        neighbor_locs = list(self.neighbhorhood[1])
-        
-        if 1 in neighbor_states:
-            light_blue_index = neighbor_states.index(1)
-            
-            count_black = 0
-            for i, s in enumerate(neighbor_states):
-                if s == 4 and i != light_blue_index:
-                    count_black += 1
 
-            if count_black == 0:
-                return True, light_blue_index 
-        return False, None
+
+
+
