c7f6d1b0d2aab0f73aaa0f1283ade0acb6add2ef
Venuk853 <84994112+Venuk853@users.noreply.github.com>
2025-04-15 16:42:24 -0500

    Update Dark_Blue_State.py

--- Dark_Blue_State.py
+++ Dark_Blue_State.py
@@ -1,33 +1,11 @@
-'States: 0:white, 1:light_blue, 2: Dark_blue, 3: red, 4:black'
+from State_Class import State
 
-import time
-from state_class import state
+class Dark_Blue(State):
+    def __init__(self, row, col,parent_array, state = 4):
+        super().__init__(row, col,parent_array, state = 4)  # Initialize parent class attributes
 
-class Dark_Blue(state):
-    state = 2
-
-
-#Notes about dark blue:
-
-#After the light blue state becomes dark blue delay 1 second
-time.sleep(1)
-
-#After delay, check if there are any other dark blue states nearby
-
-
-
-#if there are not any dark blue states nearby, move the dark blue state to a surrounding red state
-def action(self, i, j):
-    if self.state in self.neighbor:
-        return True
-
-    elif 3 in self.neighborhood:
-        index_of = self.neighbhorhood.index(3)
-    return False, index_of
-
-
-
-#This chain continues until another dark blue state is met in its neighborhood
-
-
-#This chain continues until another dark blue state is met in its neighborhood
+    def update_dark_blue(self):
+        if 3 in self.neighborhood:
+            return True
+        else:
+            return False
