b56c4a269d489509d2898ca794c787a962eea0c4
Venuk853 <84994112+Venuk853@users.noreply.github.com>
2025-04-15 16:41:17 -0500

    Update Red.py

--- Red.py
+++ Red.py
@@ -1,7 +1,10 @@
 'States: 0:white, 1:light_blue, 2: Dark_blue, 3: red, 4:black'
 from Neighborhood import neighbor as nbh
-class red:
-    state = 3
+from State_Class import State
+
+class Red(State):
+    def __init__(self, row, col, parent_array, state = 3):
+        super().__init__( row, col, parent_array, state = 3)  # Initialize parent class attributes
 
 def action(self,i,j):
     if 2 in self.neighborhood:
@@ -9,6 +12,10 @@ def action(self,i,j):
     else:
         return False
 #refrence function when red turns into dark blue
+
+
+
+
 
 
 
